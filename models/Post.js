2af18d4449ce5ed9915e354261feee45a9cd93a5
Emilio Depena <emiliosdep@gmail.com>
2020-07-07 07:49:13 -0400

    changed post model

--- models/Post.js
+++ models/Post.js
@@ -6,22 +6,50 @@ const PostSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref:'users'
     },
+    recipeTitle: {
+        type:String,
+        required: true
+    },
     text:{
         type:String,
         required:true
     },
-    model:{
+    name:{
         type:String
     },
-    year:{
-        type:Number 
+    avatar:{
+        type:String 
     },
-    price:{
-        type: Number
-        },
-    location:{
-        type: String
-    },
+    likes:[
+        {
+        user:{
+            type:Schema.Types.ObjectId,
+            ref:'users'
+        }
+        }
+    ],
+    comments:[
+        {
+            user:{
+                type:Schema.Types.ObjectId,
+                ref:'users'
+            },
+            text:{
+                type:String,
+                required:true
+            },
+            name:{
+                type:String
+            },
+            avatar:{
+                type:String 
+            },
+            date:{
+                type:Date,
+                default:Date.now
+            }
+        }
+    ],
     date:{
         type:Date,
         default:Date.now
